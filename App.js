d6708a920693db71f31c4ac5bd3fbb401c26e1c5
Harsha Senaratne <senaratneh@gmail.com>
2022-05-31 03:31:10 +0530

    API KEY Changes

--- App.js
+++ App.js
@@ -13,15 +13,23 @@ import { theme } from "./src/infrastructure/theme";
 import { Navigation } from "./src/infrastructure/navigation";
 
 import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
-
+import {
+  FIREBASE_APP_API_KEY,
+  FIREBASE_APP_AUTH_DOMAIN,
+  FIREBASE_APP_PROJECT_ID,
+  FIREBASE_APP_STORAGE_BUCKET,
+  FIREBASE_APP_MESSAGING_SENDER_ID,
+  FIREBASE_APP_APP_ID,
+  FIREBASE_APP_MEASUREMENT_ID,
+} from "@env";
 const firebaseConfig = {
-  apiKey: "AIzaSyB0C6VvJq-d_2wJcWQni4rlMAaFGg6U_mk",
-  authDomain: "happymeal-5e5e2.firebaseapp.com",
-  projectId: "happymeal-5e5e2",
-  storageBucket: "happymeal-5e5e2.appspot.com",
-  messagingSenderId: "1041321981643",
-  appId: "1:1041321981643:web:108d800daad5eb14824d4e",
-  measurementId: "G-SGD9PM9D71",
+  apiKey: FIREBASE_APP_API_KEY,
+  authDomain: FIREBASE_APP_AUTH_DOMAIN,
+  projectId: FIREBASE_APP_PROJECT_ID,
+  storageBucket: FIREBASE_APP_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_APP_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_APP_ID,
+  measurementId: FIREBASE_APP_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
